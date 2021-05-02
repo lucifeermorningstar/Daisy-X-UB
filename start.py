785d3fa70f8aeb029b3632994f138b43e0b915f5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-02 12:48:18 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system 
-system ("git clone https://GitHub.com/TheUnknownKanger/DaisyX-UB && cd DaisyX-UB && python3 -m DaisyX")
+system ("git clone https://GitHub.com/TheUnknownKanger/DaisyX-UB && cd DaisyX-UB && python -m DaisyX")
 # try af
 print("𝙾𝙷 𝚂𝙺𝙴𝙼 𝚂𝚈𝚂𝚃𝙴𝙼 𝙶𝙾𝚃 𝙲𝚁𝙰𝚂𝙷𝙴𝙳")
-system("python3 -m DaisyX")
+system("python -m DaisyX")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @DaisySupport_Official")
